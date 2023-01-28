0e3a4ebc7f2aadfe7635afd18c6bd5048177043a
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-28 10:09:21 +0100

    ADD context to describe factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,6 +6,14 @@ describe Solver do
     end
 
     describe '#Factorial' do
+        context 'When a 0+ number is given' do
+            it 'evaluates the number and returns the Factorial' do
+                expect(@solver.factorial(7)).to eq(5040)
+            end
 
+            it 'evaluatess the number and returns 1 if the number is 0' do
+                expect(@solver.factorial(0)).to eq(1)
+            end
+        end
     end 
 end
