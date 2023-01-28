f269ef41aa7f43b4e4d5315ef05a9cc11b5073a7
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-28 15:26:38 +0500

    To check the statement if FizzBuzz %3 and %5 equal to zero the return ('fizzbuzz'), and if only %3 equal to zero then return fizz, otherwise return buzz

--- solver.rb
+++ solver.rb
@@ -12,6 +12,12 @@ class Solver
     def fizzbuzz(num)
         if (num % 3).zero? && (num % 5).zero?
             'fizzbuzz'
-        
+        elsif (num % 3).zero?
+            'fizz'
+        elsif (num % 5).zero?
+            'buzz'
+        else
+            num.to_s
+        end
     end
 end
